bd2c44a51b8cc407bf612ddec1e92a0d5a970659
likedan <likedan5@icloud.com>
2015-02-25 16:07:13 -0600

    first

---
+++ WeatherAroundUs/WeatherAroundUs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAroundUs
+//
+//  Created by Kedan Li on 15/2/25.
+//  Copyright (c) 2015年 Kedan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
