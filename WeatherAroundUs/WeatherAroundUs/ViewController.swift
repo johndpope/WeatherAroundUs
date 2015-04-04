e177524541c1732156311650421a5ec0ef70b842
likedan <likedan5@icloud.com>
2015-04-04 16:43:13 -0500

    pull out city list

--- WeatherAroundUs/WeatherAroundUs/ViewController.swift
+++ WeatherAroundUs/WeatherAroundUs/ViewController.swift
@@ -14,20 +14,39 @@ class ViewController: UIViewController {
     @IBOutlet var cityDetail: UIView!
     @IBOutlet var cityList: UIView!
 
-    var draggingGesture: UIPanGestureRecognizer!
+    var draggingGesture: UIScreenEdgePanGestureRecognizer!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cityDetail.frame.size = view.frame.size
-        cityDetail.frame.origin = CGPointMake(0, self.view.frame.height * 3 / 4)
+        var screenSwipeReco: UIScreenEdgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "screenLeftEdgeSwiped:")
+        screenSwipeReco.edges = UIRectEdge.Left
+        self.view.addGestureRecognizer(screenSwipeReco)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func viewDidAppear(animated: Bool) {
         cityDetail.frame.size = view.frame.size
         cityDetail.frame.origin = CGPointMake(0, self.view.frame.height * 3 / 4)
+        
+        cityList.frame.size = CGSizeMake(cityList.frame.size.width / 2 , cityList.frame.size.height)
+        cityList.frame.origin = CGPointMake(-self.view.frame.width, 0)
+    }
+    
+    func screenLeftEdgeSwiped(sender: UIScreenEdgePanGestureRecognizer) {
+        
+        var x = sender.translationInView(view).x
+        cityList.frame.origin = CGPointMake(x - cityList.frame.width, 0)
+
+        if cityList.frame.origin.x >= 0 || sender.state == UIGestureRecognizerState.Ended || sender.state == UIGestureRecognizerState.Failed ||
+            sender.state == UIGestureRecognizerState.Cancelled {
+        
+            UIView.animateWithDuration(Double(x / cityList.frame.width * 0.6), animations: { () -> Void in
+                self.cityList.frame.origin = CGPointMake(0, 0)
+            })
+
+        }
     }
     
     override func didReceiveMemoryWarning() {
