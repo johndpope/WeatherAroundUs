1b6cb7292090a567bb526052ac3b901af9eaf52a
likedan <likedan5@icloud.com>
2015-04-04 16:58:32 -0500

    add dragger

--- WeatherAroundUs/WeatherAroundUs/ViewController.swift
+++ WeatherAroundUs/WeatherAroundUs/ViewController.swift
@@ -20,9 +20,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var screenSwipeReco: UIScreenEdgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "screenLeftEdgeSwiped:")
-        screenSwipeReco.edges = UIRectEdge.Left
-        self.view.addGestureRecognizer(screenSwipeReco)
+        var cityListAppearDragger: UIScreenEdgePanGestureRecognizer = UIScreenEdgePanGestureRecognizer(target: self, action: "cityListAppear:")
+        cityListAppearDragger.edges = UIRectEdge.Left
+        self.view.addGestureRecognizer(cityListAppearDragger)
+        
+        var cityListDisappearDragger: UIPanGestureRecognizer = UIPanGestureRecognizer(target: self, action: "cityListDisappear:")
+        self.cityList.addGestureRecognizer(cityListDisappearDragger)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -34,18 +37,47 @@ class ViewController: UIViewController {
         cityList.frame.origin = CGPointMake(-self.view.frame.width, 0)
     }
     
-    func screenLeftEdgeSwiped(sender: UIScreenEdgePanGestureRecognizer) {
+    func cityListAppear(sender: UIScreenEdgePanGestureRecognizer) {
         
         var x = sender.translationInView(view).x
         cityList.frame.origin = CGPointMake(x - cityList.frame.width, 0)
 
-        if cityList.frame.origin.x >= 0 || sender.state == UIGestureRecognizerState.Ended || sender.state == UIGestureRecognizerState.Failed ||
+        if cityList.frame.origin.x >= -50 {
+            sender.enabled = false
+        }
+        
+        if sender.state == UIGestureRecognizerState.Ended || sender.state == UIGestureRecognizerState.Failed ||
             sender.state == UIGestureRecognizerState.Cancelled {
         
-            UIView.animateWithDuration(Double(x / cityList.frame.width * 0.6), animations: { () -> Void in
+            UIView.animateWithDuration(Double(x / cityList.frame.width * 1), animations: { () -> Void in
                 self.cityList.frame.origin = CGPointMake(0, 0)
+                }, completion: { (bool) -> Void in
+                    sender.enabled = true
             })
 
+        }
+    }
+    
+    func cityListDisappear(sender: UIPanGestureRecognizer) {
+        var x = sender.translationInView(cityList).x
+        println(x)
+        if x < 0 {
+            cityList.frame.origin = CGPointMake(x, 0)
+            
+            if cityList.frame.origin.x <= -cityList.frame.width + 50 {
+                sender.enabled = false
+            }
+            
+            if sender.state == UIGestureRecognizerState.Ended || sender.state == UIGestureRecognizerState.Failed ||
+                sender.state == UIGestureRecognizerState.Cancelled {
+                    
+                    UIView.animateWithDuration(Double(-x / cityList.frame.width * 1), animations: { () -> Void in
+                        self.cityList.frame.origin = CGPointMake(-self.cityList.frame.width, 0)
+                        }, completion: { (bool) -> Void in
+                            sender.enabled = true
+                    })
+                    
+            }
         }
     }
     
