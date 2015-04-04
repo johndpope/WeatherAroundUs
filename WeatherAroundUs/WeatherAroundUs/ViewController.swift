80c07486a137251c09004027da75a30eaf40b18c
likedan <likedan5@icloud.com>
2015-04-04 15:59:49 -0500

    scrollview

--- WeatherAroundUs/WeatherAroundUs/ViewController.swift
+++ WeatherAroundUs/WeatherAroundUs/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cityDetailBack: UIScrollView!
+    @IBOutlet var cityDetail: UIView!
+
+    var draggingGesture: UIPanGestureRecognizer!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        draggingGesture = UIPanGestureRecognizer(target: self, action: "draggedDetailView:")
+        cityDetail.addGestureRecognizer(draggingGesture)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func draggedDetailView(sender: UIPanGestureRecognizer){
+    
+        var y = sender.translationInView(view).y
+        
+        // check if the view is fully display
+        if sender.view!.frame.origin.y == 0{
+            
+            
+        }else{
+            
+            if y < 0 {
+                println("move")
+            }
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
